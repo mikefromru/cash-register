3669768b7146a2177ad42d260c7187002133cbe8
mikefromru <googlockgood@gmail.com>
2023-12-11 20:22:48 +0300

    Added saving to PDF file

--- app/views.py
+++ app/views.py
@@ -22,28 +22,28 @@ def load_bill(request):
     ids = request.data.get('items', None)
     queryset = Item.objects.filter(pk__in=ids)
 
-    pdf_ = fpdf.FPDF(format='letter') #pdf format
-    pdf_.add_page() #create new page
+    pdf = fpdf.FPDF(format='letter') #pdf format
+    pdf.add_page() #create new page
     path = os.path.abspath('app/OpenSans/OpenSans-Bold.ttf')
-    pdf_.add_font('OpenSans', '', path, uni=True)
-    pdf_.set_font('OpenSans', size=12) # font and textsize
-    pdf_.cell(200, 10, txt='ООО "КОМПАНИЯ К"', ln=1, align='T')
-    pdf_.cell(200, 10, txt='Добро пожаловать', ln=1, align='T')
-    pdf_.cell(200, 10, txt='ККМ 00075411 #3969', ln=1, align='T')
-    pdf_.cell(200, 10, txt='ЭКЛЗ 3851495566', ln=1, align='T')
-    pdf_.cell(200, 10, txt='ДАТА СИС', ln=1, align='T')
-    pdf_.cell(200, 10, txt=formtatted_datetime, ln=1, align='T')
+    pdf.add_font('OpenSans', '', path, uni=True)
+    pdf.set_font('OpenSans', size=12) # font and textsize
+    pdf.cell(200, 10, txt='ООО "КОМПАНИЯ К"', ln=1, align='T')
+    pdf.cell(200, 10, txt='Добро пожаловать', ln=1, align='T')
+    pdf.cell(200, 10, txt='ККМ 00075411 #3969', ln=1, align='T')
+    pdf.cell(200, 10, txt='ЭКЛЗ 3851495566', ln=1, align='T')
+    pdf.cell(200, 10, txt='ДАТА СИС', ln=1, align='T')
+    pdf.cell(200, 10, txt=formtatted_datetime, ln=1, align='T')
 
     total = 0
     for x in queryset:
-        pdf_.cell(200, 10, txt=f'{x.title} = {x.price}', ln=2, align='L')
+        pdf.cell(200, 10, txt=f'{x.title} = {x.price}', ln=2, align='L')
         total += x.price
 
-    pdf_.cell(200, 10, txt=f'ИТОГ {total}', ln=1, align='T')
-    pdf_.cell(200, 10, txt=f'НАЛИЧНЫМИ = {total}', ln=1, align='T')
-    pdf_.cell(200, 10, txt='****************************', ln=1, align='T')
-    pdf_.cell(200, 10, txt='000003751# 05970', ln=1, align='T')
+    pdf.cell(200, 10, txt=f'ИТОГ {total}', ln=1, align='T')
+    pdf.cell(200, 10, txt=f'НАЛИЧНЫМИ = {total}', ln=1, align='T')
+    pdf.cell(200, 10, txt='****************************', ln=1, align='T')
+    pdf.cell(200, 10, txt='000003751# 05970', ln=1, align='T')
 
-    pdf_.output('media/чек.pdf')
+    pdf.output('media/чек.pdf')
 
     return Response('msg')
