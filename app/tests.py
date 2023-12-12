0c7f9506816eb688f10fb4aee77eadc05c3ed5e8
mikefromru <googlockgood@gmail.com>
2023-12-12 22:38:31 +0300

    Added tests for model

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Item
+import decimal
 
-# Create your tests here.
+class ItemTestCase(TestCase):
+
+    def setUp(self):
+        Item.objects.create(title='Bread', price=45.50)
+        Item.objects.create(title='Winstone', price=205.50)
+    
+    def test_title_islower(self):
+        bread = Item.objects.get(title='Bread')
+        self.assertTrue(bread.title.capitalize())
+
+    def test_price_isdecimal(self):
+        winstone = Item.objects.get(title='Winstone')
+        print(type(winstone.price))
+        self.assertEqual(True, isinstance(winstone.price, decimal.Decimal))
+        
