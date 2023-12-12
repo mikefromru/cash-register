28636ae44b7dfb6115182752641d0d47f26b0916
mikefromru <googlockgood@gmail.com>
2023-12-13 01:39:47 +0300

    Added README.md

--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.17']
+ALLOWED_HOSTS =  env.list('ALLOWED_HOSTS')
 
 
 # Application definition
